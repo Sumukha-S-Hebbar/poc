da0249887151f7141164e5caae02a7aecfca95b0
Developers I4s <i4s.developers@gmail.com>
2025-10-14 11:22:57 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -32,21 +32,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: [],
-    path: '/_next/image',
-    loader: 'default',
-    loaderFile: '',
-    disableStaticImages: false,
-    minimumCacheTTL: 60,
-    formats: ['image/webp'],
-    dangerouslyAllowSVG: false,
-    contentSecurityPolicy: "script-src 'none'; frame-src 'none'; sandbox;",
-    contentDispositionType: 'inline',
-    remotePatterns: [],
-    unoptimized: false,
-
   },
 };
 
